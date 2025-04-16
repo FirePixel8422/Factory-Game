f74472111806c744c0fcfd31058cacb221f4c6cc
FirePixel8422 <bmtminecraft@hotmail.com>
2025-04-17 00:28:15 +0200

    Update InstanceRenderer.cs

--- Assets/C# Scripts/Static Managers/InstanceRenderer.cs
+++ Assets/C# Scripts/Static Managers/InstanceRenderer.cs
@@ -6,29 +6,6 @@ using UnityEngine;
 [BurstCompile(DisableSafetyChecks = true, OptimizeFor = OptimizeFor.Performance)]
 public class InstanceRenderer
 {
-    private Mesh[] meshes;
-
-    //stores material
-    private RenderParams renderParams;
-
-    private NativeArray<MeshDrawData> meshData;
-
-    private NativeArray<Matrix4x4> matrixData;
-    private int matrixId;
-
-    private NativeArray<Matrix4x4> meshMatrices;
-
-    public void AddMeshData(int meshId, Matrix4x4 matrix)
-    {
-        matrixData[matrixId] = matrix;
-
-        meshData[meshId].AddMatrixId(matrixId);
-
-        matrixId += 1;
-    }
-
-
-
     public InstanceRenderer(Material mat)
     {
         renderParams = new RenderParams()
@@ -37,7 +14,6 @@ public class InstanceRenderer
 
             shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On,
             receiveShadows = true,
-            lightProbeUsage = UnityEngine.Rendering.LightProbeUsage.Off,
 
             motionVectorMode = MotionVectorGenerationMode.ForceNoMotion,
 
@@ -52,28 +28,31 @@ public class InstanceRenderer
     }
 
 
+
+    private Mesh[] meshes;
+
+    //stores material
+    private RenderParams renderParams;
+
+    private NativeArray<Matrix4x4> meshInstanceMatrices;
+    private NativeArray<int> meshInstanceMatrixCounts;
+
+
+
+
+    public void Dispose()
+    {
+        
+    }
+
+
     [BurstCompile(DisableSafetyChecks = true, OptimizeFor = OptimizeFor.Performance)]
     private void OnUpdate()
     {
         for (int meshIndex = 0; meshIndex < meshData.Length; meshIndex++)
         {
-            MeshDrawData cMeshDrawData = meshData[meshIndex];
-
-            int matrixCount = cMeshDrawData.MatrixCount;
-
-            if (matrixCount == 0)
-            {
-                continue;
-            }
-
-            // Copy the relevant matrices into the temporary array
-            for (int i2 = 0; i2 < matrixCount; i2++)
-            {
-                meshMatrices[i2] = matrixData[cMeshDrawData.matrixIds[i2]];
-            }
-
             // Render the instances
-            Graphics.RenderMeshInstanced(default, meshes[meshIndex], 0, meshMatrices, matrixCount);
+            Graphics.RenderMeshInstanced(renderParams, meshes[meshIndex], 0, meshInstanceMatrices, meshInstanceMatrixCounts[meshIndex], );
         }
     }
 }
